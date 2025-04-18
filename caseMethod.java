95937dfd36a0375e0361c9375e811f025722c9af
Adellia03 <salsadellia736@gmail.com>
2025-04-18 20:13:33 +0700

    Add files via upload

--- caseMethod.java
+++ caseMethod.java
@@ -1,3 +1,9 @@
 public class caseMethod {
     // diisi buat kumpulan method atau fungsi
+    public static void tampilMahasiswa() {
+        System.out.println("=== Daftar Mahasiswa ===");
+        System.out.println("NIM: 22001 | Nama: Ali Rahman      | Prodi: Informatika");
+        System.out.println("NIM: 22002 | Nama: Budi Santoso    | Prodi: Informatika");
+        System.out.println("NIM: 22003 | Nama: Citra Dewi      | Prodi: Sistem Informasi Bisnis");
+    }
 }
--- caseMethodMain.java
+++ caseMethodMain.java
@@ -1,6 +1,44 @@
+import java.util.Scanner;
 public class caseMethodMain {
-
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        int pilihan;
+        do {
+            System.out.println("=== MENU SISTEM AKADEMIK ===");
+            System.out.println("1. Tampilkan Daftar Mahasiswa");
+            System.out.println("2. Tampilkan Daftar Mata Kuliah");
+            System.out.println("3. Tampilkan Data Penilaian");
+            System.out.println("4. Urutkan Mahasiswa Berdasarkan Nilai Akhir");
+            System.out.println("5. Cari Mahasiswa Berdasarkan NIM");
+            System.out.println("0. Keluar");
+            System.out.print("Pilih menu: ");
+            pilihan = sc.nextInt();
+            sc.nextLine(); 
+
+            switch (pilihan) {
+                case 1:
+                   caseMethod.tampilMahasiswa();
+                    break;
+                case 2:
+                    tampilMataKuliah(); 
+                    break;
+                case 3:
+                    tampilPenilaian(); 
+                    break;
+                case 4:
+                    urutkanMahasiswaBerdasarkanNilaiAkhir(); 
+                    break;
+                case 5:
+                    cariMahasiswaBerdasarkanNIM(); 
+                    break;
+                case 0:
+                    System.out.println("Program selesai. Terima kasih");
+                    break;
+                default:
+                    System.out.println("Pilihan tidak valid. Coba lagi.");
+            }
+        } while (pilihan != 0);
     }
+        
 }
